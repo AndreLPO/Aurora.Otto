cbdd32447efa9932b5585b7bd248a96b323bf9cd
André <andrelpo573@gmail.com>
2026-03-24 08:03:30 -0300

    AOBP-3: Adicionado Raycast no GroundCheck

--- Assets/Scripts/Aurora.cs
+++ Assets/Scripts/Aurora.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class Aurora : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
+    public float moveSpeed = 3f;
+    public float jumpForce = 6f;
+
+    // public Transform groundCheck;
+    // public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
-    private bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,17 +20,24 @@ public class Aurora : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(transform.position, Vector2.down * 1.5f, Color.red);
+
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if(Input.GetKeyDown(KeyCode.Space) && GetIsGrounded())
         {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    private void FixedUpdate()
+    private bool GetIsGrounded()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        return Physics2D.Raycast(transform.position, Vector2.down, 1.5f, groundLayer);
     }
+
+    // private void FixedUpdate()
+    // {
+    //     isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    // }
 }
