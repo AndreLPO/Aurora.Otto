09fd0fa231c2973eb64f1d7b42958100b384eccc
André <andrelpo573@gmail.com>
2026-03-23 22:25:48 -0300

    feat/AOBP-3: Adicionado cenário simples, o objeto Aurora e uma movimentação simples (mover para os lados e pular), ground check simples via Transform. Objetos com coliders e gravidade simples da Unity

---
+++ Assets/Scripts/Aurora.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Aurora : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+}
