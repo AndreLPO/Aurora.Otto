468e68a1f6906abc644ec600343cc1f17e337bfd
André <andrelpo573@gmail.com>
2026-04-15 22:06:43 -0300

    AOBP-10 e AOBP-11: Sistema do botão para abrir a porta

--- Assets/Scripts/PressButton.cs
+++ Assets/Scripts/PressButton.cs
@@ -3,20 +3,30 @@ using UnityEngine;
 public class PressButton : MonoBehaviour
 {
     [SerializeField] private LayerMask ottoLayer;
+    [SerializeField] private Transform door;
+
 
     void Start()
     {}
 
     void Update()
     {
-        if (GetIsUnderOtto())
-        {
-            SetColor(Color.red);
-        }
-        else
-        {
-            SetColor(Color.yellow);
-        }
+        // if (GetIsUnderOtto())
+        // {
+        //     SetColor(Color.red);
+        //     door.localPosition = new Vector2(door.localPosition.x, -0.5f);
+        // }
+        // else
+        // {
+        //     SetColor(Color.yellow);
+        //     door.localPosition = new Vector2(door.localPosition.x, 0.5f);
+        // }
+        float targetY = GetIsUnderOtto() ? 3.5f : 0;
+        door.localPosition = Vector2.MoveTowards(
+            door.localPosition,
+            new Vector2(door.localPosition.x, targetY),
+            2f * Time.deltaTime
+        );
     }
 
     private bool GetIsUnderOtto()
